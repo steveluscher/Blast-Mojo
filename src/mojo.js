18858f25a237f2694f9a128488e13321c4eace9c
Jaime Bueza <jbueza@gmail.com>
2010-10-17 08:43:33 -0700

    Style fixes

--- src/mojo.js
+++ src/mojo.js
@@ -1,34 +1,34 @@
 /*
-	Class: mojo
-	Note: These functions reside within the mojo.* namespace.
+  Class: mojo
+  Note: These functions reside within the mojo.* namespace.
 */
 var mojo = {
-	version: "1.1.4",
-	/*
-	Function: evaluateClassPath
+  version: "2.0",
+  /*
+  Function: evaluateClassPath
 
-	Returns the variable denoted by a dot-separated classpath
+  Returns the variable denoted by a dot-separated classpath
 
-	Parameters:
-		classPath - {string}
+  Parameters:
+    classPath - {string}
 
-	Returns:
-		{Mixed} variable
+  Returns:
+    {Mixed} variable
 
-	Example:
-		(start code)
-		// instantiate an object by classpath without using eval
-		var myObjectClassPath = 'stdlib.controller.TemplateController';
-		var myObjectInstance = new mojo.evaluateClassPath(myObjectClassPath)();
-		(end)
-	*/
-	evaluateClassPath: function(classPath) {
-		var classPathParts = classPath.split('.');
+  Example:
+    (start code)
+    // instantiate an object by classpath without using eval
+    var myObjectClassPath = 'stdlib.controller.TemplateController';
+    var myObjectInstance = new mojo.evaluateClassPath(myObjectClassPath)();
+    (end)
+  */
+  evaluateClassPath: function(classPath) {
+    var classPathParts = classPath.split('.');
 
-		var variable = window;
-		for(var len=classPathParts.length, i=0; i < len; i++) {
-		  if(variable) variable = variable[classPathParts[i]];
-		}
-		return variable;
-	}
+    var variable = window;
+    for (var len = classPathParts.length, i = 0; i < len; i++) {
+      if (variable) variable = variable[classPathParts[i]];
+    }
+    return variable;
+  }
 };
